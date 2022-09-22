a48678415668fa046f8991d62306da47543b39cf
Bryan <bryanlima200@outlook.com>
2022-09-22 02:00:41 -0300

    Iniciado implementação de notificações

---
+++ src/ProdutoStore.Business/Interfaces/Notificacoes/INotificador.cs
@@ -0,0 +1,18 @@
+﻿using ProdutoStore.Business.Notificacoes;
+using System.Collections.Generic;
+
+namespace ProdutoStore.Business.Interfaces.Notificacoes
+{
+    public interface INotificador
+    {
+        #region Métodos Públicos
+
+        bool TemNotificacao();
+
+        List<Notificacao> ObterNotificacoes();
+
+        void Handle(Notificacao notificacao);
+
+        #endregion Métodos Públicos
+    }
+}
---
+++ src/ProdutoStore.Business/Notificacoes/Notificacao.cs
@@ -0,0 +1,12 @@
+﻿namespace ProdutoStore.Business.Notificacoes
+{
+    public class Notificacao
+    {
+        public Notificacao(string mensagem)
+        {
+            Mensagem = mensagem;
+        }
+
+        public string Mensagem { get; set; }
+    }
+}
