f297315b2081400ec6d0c1407bfcabc6af277434
Bryan <bryanlima200@outlook.com>
2022-09-21 21:15:57 -0300

    Adicionado projeto ProdutoStore.AppMvc

---
+++ src/ProdutoStore.AppMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProdutoStore.AppMvc.Startup))]
+namespace ProdutoStore.AppMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
