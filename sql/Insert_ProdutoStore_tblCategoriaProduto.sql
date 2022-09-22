041e05e9f1091122d04b8191fe103cf3788a908d
Bryan <bryanlima200@outlook.com>
2022-09-21 23:14:21 -0300

    Adicionado script SQL Insert_ProdutoStore_tblCategoriaProduto

---
+++ sql/Insert_ProdutoStore_tblCategoriaProduto.sql
@@ -0,0 +1,7 @@
+USE [ProdutoStore]
+
+INSERT INTO [dbo].[tblCategoriaProduto] ([Nome], [Descricao], [Ativo]) VALUES ('Eletrônico', 'Eletrodomésticos', 1)
+INSERT INTO [dbo].[tblCategoriaProduto] ([Nome], [Descricao], [Ativo]) VALUES ('Informática', 'Produtos para Informática', 1)
+INSERT INTO [dbo].[tblCategoriaProduto] ([Nome], [Descricao], [Ativo]) VALUES ('Celulares', 'Aparelhos e acessórios', 1)
+INSERT INTO [dbo].[tblCategoriaProduto] ([Nome], [Descricao], [Ativo]) VALUES ('Moda', 'Artigos para vestuário em geral', 1)
+INSERT INTO [dbo].[tblCategoriaProduto] ([Nome], [Descricao], [Ativo]) VALUES ('Livros', 'Livros', 1)
