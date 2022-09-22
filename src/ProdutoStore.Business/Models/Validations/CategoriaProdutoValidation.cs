5d193f409af50853ba7a8c417ba2114406bfb537
Bryan <bryanlima200@outlook.com>
2022-09-22 00:35:32 -0300

    Adicionado validations

---
+++ src/ProdutoStore.Business/Models/Validations/CategoriaProdutoValidation.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace ProdutoStore.Business.Models.Validations
+{
+    public class CategoriaProdutoValidation : AbstractValidator<CategoriaProduto>
+    {
+        public CategoriaProdutoValidation()
+        {
+            RuleFor(categoriaProduto => categoriaProduto.Nome)
+                .NotEmpty()
+                .WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 250)
+                .WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+            RuleFor(categoriaProduto => categoriaProduto.Descricao)
+                .NotEmpty()
+                .WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 250)
+                .WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+        }
+    }
+}
---
+++ src/ProdutoStore.Business/Models/Validations/ProdutoValidation.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace ProdutoStore.Business.Models.Validations
+{
+    public class ProdutoValidation : AbstractValidator<Produto>
+    {
+        public ProdutoValidation()
+        {
+            RuleFor(produto => produto.Nome)
+                .NotEmpty()
+                .WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 250)
+                .WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+            RuleFor(produto => produto.Descricao)
+                .NotEmpty()
+                .WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 250)
+                .WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+        }
+    }
+}
